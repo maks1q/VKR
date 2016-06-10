f3d8f90c9eb47d19ee8f1db8b61e72aa0d2def73
unknown <@KMA-VKR-1.kma-vkr.com>
2016-06-10 12:27:54 +0700

    4

--- js/scripts.js
+++ js/scripts.js
@@ -12,25 +12,9 @@ $(document).ready(function(){
 			});	
 	})
 	
-	$(".btn-magaz").click(function(){
-		id = $(this).data("id"); 
-		$(".btn-magaz").removeClass('btn-magaz');
-		$.ajax({
-				type: "GET",
-				url: "/medved/"+id+"/magaz",
-			})
-			.done(function( data ) {			
-				$("#magaz").append('<thead class="thead-default"><tr><th>#</th><th>Название магазина</th><th>Адрес</th></tr></thead><tbody>');
-				for (var mag of data.magaz) {
-					$("#magaz").append('<tr><th scope="row">'+mag.pk_magaz+'</th><td>'+mag.name_magaz+'</td><td>'+mag.adres_magaz+'</td></tr>');
-				}
-				$("#magaz").append('</tbody>');			
-			});			
-	})
-	
 	$('#deleteModal').on('show.bs.modal', function (event) {
 		var button = $(event.relatedTarget) 
-		var content = button.data('content')	
-		$(this).find('#del').attr('data-id', content);
+		var content = button.data('content')
+		$(this).find('#del').data("id", content);
 	})
 })
