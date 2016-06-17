87cbe867cb249da6411675b589f9aa425d3515eb
unknown <@KMA-VKR-1.kma-vkr.com>
2016-06-17 14:14:32 +0700

    автоперезагрузка и выбор типа файла

--- index.php
+++ index.php
@@ -55,10 +55,11 @@ $sapp->post('/newdisk', function (Application $app, Request $req) {
     $name = $req->get('disk-name');
 	if($name == null) $name = 'Диск';
 	$desc = $req->get('disk-description');
-	$type = 'CD';
+	$type = '...';
 	$user = getUser($conn);	
+	$size_disk = 0;
     $conn->insert('disk', ['name_disk' => $name, 'description_disk' => $desc, 'fk_user' => $user["pk_user"],
-	'type_disk' => $type, 'status_disk' => NOT_RECORDED, 'status_string_disk' => NOT_RECORDED_STRING]);
+	'type_disk' => $type, 'size_disk' => $size_disk, 'status_disk' => NOT_RECORDED, 'status_string_disk' => NOT_RECORDED_STRING]);
 	$id = $conn->lastInsertId();
 	mkdir("./files/".$user["pk_user"]."/".$id, 0700);
 	$uploaddir = './files/'.$user["pk_user"].'/'.$id.'/';
@@ -68,44 +69,21 @@ $sapp->post('/newdisk', function (Application $app, Request $req) {
 			$tmp_name = $_FILES["files"]["tmp_name"][$key];
 			$type = $_FILES["files"]["type"][$key];
 			$size = $_FILES["files"]["size"][$key];
+			$size_disk = $size_disk + $size;
 			copy($tmp_name, $uploadfile);
-			$conn->insert('file', ['name_file' => basename($_FILES["files"]["name"][$key]), 'path_file' => $_FILES["files"]["name"][$key], 'size_file' => $size, 'type_file' => $type, 'fk_disk' => $id]);
+			$conn->insert('file', ['name_file' => basename($_FILES["files"]["name"][$key]), 'size_file' => $size, 'type_file' => $type, 'fk_disk' => $id]);
 		}
-	}	
+	}
+	if($size_disk < 700000000) {
+		$type = 'CD';
+	}
+	else {
+		$type = 'DVD';
+	}
+	$conn->update('disk', ['type_disk' => $type, 'size_disk' => $size_disk], ['pk_disk' => $id]);
     return $app->redirect('/');
 });
 
-//получить страницу "Файлы"
-/*
-$sapp->get('/loadfiles/{id}', function (Application $app, $id) {
-	$conn = $app['db'];
-	$login = $_SERVER['REMOTE_USER'];
-	$encod = mb_detect_encoding($login, "windows-1251");
-	if($encod == "Windows-1251") $login = iconv('Windows-1251','utf-8', $login);
-	$user = $conn->fetchAssoc('select * from user where login_user = ?', [$login]);
-	$disk = $conn->fetchAssoc('select * from disk where pk_disk = ?', [$id]);
-    return $app['twig']->render('loadfiles.html', ['id' => $id]);
-});
-
-//загрузить файлы на сервер
-$sapp->post('/loadfiles/{id}', function (Application $app, Request $req, $id) {
-	$conn = $app['db'];
-	$login = $_SERVER['REMOTE_USER'];
-	$encod = mb_detect_encoding($login, "windows-1251");
-	if($encod == "Windows-1251") $login = iconv('Windows-1251','utf-8', $login);
-	$user = $conn->fetchAssoc('select * from user where login_user = ?', [$login]);	
-	$uploaddir = './files/'.$user["pk_user"].'/'.$id.'/';
-	foreach ($_FILES["uploads"]["error"] as $key => $error) {
-		if ($error == UPLOAD_ERR_OK) {
-			$uploadfile = $uploaddir.basename($_FILES["uploads"]["name"][$key]);
-			$tmp_name = $_FILES["uploads"]["tmp_name"][$key];
-			copy($tmp_name, $uploadfile);
-			$conn->insert('file', ['path_file' => basename($_FILES["uploads"]["name"][$key]), 'fk_disk' => $id]);
-		}
-	}	
-    return $app->redirect('/');
-});*/
-	
 //получить страницу "Настройки"
 $sapp->get('/settings', function (Application $app) {
 	$conn = $app['db'];
@@ -148,8 +126,9 @@ $sapp->post('/editdisk/{id}', function (Application $app, Request $req, $id) {
 	$name = $req->get('disk-name');
 	if($name == null) $name = 'Диск';
 	$desc = $req->get('disk-description');
-	$type = 'CD';
 	$user = getUser($conn);	
+	$disk = $conn->fetchAssoc('select * from disk where pk_disk = ?', [$id]);
+	$size_disk = $disk["size_disk"];
 	$conn->update('disk', ['name_disk' => $name, 'description_disk' => $desc], ['pk_disk' => $id]);
 	$uploaddir = './files/'.$user["pk_user"].'/'.$id.'/';
 	foreach ($_FILES["files"]["error"] as $key => $error) {
@@ -158,10 +137,18 @@ $sapp->post('/editdisk/{id}', function (Application $app, Request $req, $id) {
 			$tmp_name = $_FILES["files"]["tmp_name"][$key];
 			$type = $_FILES["files"]["type"][$key];
 			$size = $_FILES["files"]["size"][$key];
+			$size_disk = $size_disk + $size;
 			copy($tmp_name, $uploadfile);
-			$conn->insert('file', ['name_file' => basename($_FILES["files"]["name"][$key]), 'path_file' => $_FILES["files"]["name"][$key], 'size_file' => $size, 'type_file' => $type, 'fk_disk' => $id]);
+			$conn->insert('file', ['name_file' => basename($_FILES["files"]["name"][$key]), 'size_file' => $size, 'type_file' => $type, 'fk_disk' => $id]);
 		}
-	}	
+	}
+	if($size_disk < 700000000) {
+		$type = 'CD';
+	}
+	else {
+		$type = 'DVD';
+	}
+	$conn->update('disk', ['type_disk' => $type, 'size_disk' => $size_disk], ['pk_disk' => $id]);	
 	return $app->redirect('/');
 });
 
@@ -170,23 +157,8 @@ $sapp->get('/queue', function (Application $app) {
     $conn = $app['db'];
 	//$user = getUser($conn);
 	//$disks = $conn->fetchAll('select * from disk where fk_user = ?', [$user["pk_user"]]);
-	$date = date("Y-m-d H:i:s");
-    return $app['twig']->render('queue.html',['date' => $date]/*, 'disks' => $disks]*/);
-});
-
-/*
-$sapp->delete('/disk/{id}', function (Application $app, $id) {
-    $conn = $app['db'];
-	$conn->delete('file', ['fk_disk' => $id]);
-    $conn->delete('disk', ['pk_disk' => $id]);
-    return $app->redirect('/');
-});
-*/
-
-$sapp->get('/queue', function ($id) use ($sapp) {
-    /**@var $conn Connection */
 	$data = date("Y-m-d H:i:s");
-    return $sapp->json(array('data' => $data), 200);
+    return $app['twig']->render('queue.html',['data' => $data]/*, 'disks' => $disks]*/);
 });
 
 //AJAX удаление диска 
@@ -241,29 +213,7 @@ $sapp->post('/copy', function (Request $request) use ($sapp) {
     return $sapp->json(array('disk' => $disk1), 200);
 	//return $sapp->json("Диск скопирован!", 200);
 });
-/*
-$sapp->get('/copy/{id}', function ($id) use ($sapp) {
-    $conn = $sapp['db'];
-	$user = getUser($conn);	
-	//$id = $request->request->get('id');
-	$disk = $conn->fetchAssoc('select * from disk where pk_disk = ?', [$id]);
-	$conn->insert('disk', ['name_disk' => $disk["name_disk"], 'description_disk' => $disk["description_disk"], 'fk_user' => $disk["fk_user"],
-	'type_disk' => $disk["type_disk"], 'status_disk' => NOT_RECORDED, 'status_string_disk' => NOT_RECORDED_STRING]);
-	$id1 = $conn->lastInsertId();
-	$files = $conn->fetchAll('select * from file where fk_disk = ?', [$id]);
-	mkdir("./files/".$user["pk_user"]."/".$id1, 0700);
-	$olddir = './files/'.$user["pk_user"].'/'.$id.'/';
-	$uploaddir = './files/'.$user["pk_user"].'/'.$id1.'/';
-	foreach ($files as $f) {
-		$source = $olddir.$f["name_file"];
-		$dest = $uploaddir.$f["name_file"];
-		copy($source, $dest);
-		$conn->insert('file', ['name_file' => $f["name_file"], 'path_file' => $f["path_file"], 'size_file' => $f["size_file"], 'type_file' => $f["type_file"], 'fk_disk' => $id1]);
-	}
-	$disk1 = $conn->fetchAssoc('select * from disk where pk_disk = ?', [$id1]);
-    return $sapp->json(array('disk' => $disk1), 200);
-});
-*/
+
 //функция получения пользователя
 function getUser($c) {
     $login = $_SERVER['REMOTE_USER'];
